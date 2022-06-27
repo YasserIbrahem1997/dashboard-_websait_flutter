25e617bb98c09d6f788589e20578f442397a2ca7
Eng. Yasser Ibrahem <89817238+engyasser1997@users.noreply.github.com>
2022-06-27 15:56:13 +0200

    websait_flutter

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF081730);
+const secondaryColor = Color(0xFF232A4E);
+const bgColor = Color(0xFF03112D);
+
+const defaultPadding = 16.0;
